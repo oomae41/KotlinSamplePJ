c4129598e01dccae01314c9441c45a578f90d32f
oomae41 <99334392+jx61130@users.noreply.github.com>
2023-07-19 17:53:08 +0900

    クラスの作成まで

--- src/main/kotlin/Example.kt
+++ src/main/kotlin/Example.kt
@@ -1,3 +1,24 @@
 fun main(){
     println("Hello ことりん")
+    printText()
+    println(countLength("aaa" + "あいうえお"))
+
+    for(i in 1..5) {
+        println("i is $i")
+    }
+
+    val human = Human()
+    human.showName("masao")
+
+    val humanoid = Humanoid("masao")
+    humanoid.showName()
+}
+
+fun printText() {
+    val text = "Hello Kotlin!"
+    println(text)
+}
+
+fun countLength(str: String): Int{
+    return str.length
 }
---
+++ src/main/kotlin/Human.kt
@@ -0,0 +1,13 @@
+
+
+class Human {
+    fun showName(name: String) {
+        println(name)
+    }
+}
+
+class Humanoid(val name: String) {
+    fun showName(){
+        println(name)
+    }
+}
